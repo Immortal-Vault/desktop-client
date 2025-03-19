c753acfe15cad28c5a995980eec9c96006d4ab09
ronessie <vsakolinskaa@gmail.com>
2025-03-19 20:03:41 +0300

    chore: add localization

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -11,27 +11,27 @@ import {
     Text,
     TextInput,
 } from '@mantine/core';
-//import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { IconBrandGoogleFilled, IconBrandAppleFilled, IconBrandGithubFilled } from '@tabler/icons-react';
 import { useForm } from '@mantine/form';
 import { upperFirst, useToggle } from '@mantine/hooks';
 import { useState } from 'react';
 
 export default function LoginPage(props: PaperProps) {
-    // function LanguageSwitcher() {
-    //     const { i18n } = useTranslation();
-    //
-    //     const toggleLanguage = () => {
-    //         i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
-    //     };
-    //
-    //     return <Button onClick={toggleLanguage}>Сменить язык ({i18n.language})</Button>;
-    // }
-    // const { t } = useTranslation();
+    function LanguageSwitcher() {
+        const { i18n } = useTranslation();
+
+        const toggleLanguage = () => {
+            i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+        };
+
+        return <Button onClick={toggleLanguage}>{t('change_language')} ({i18n.language})</Button>;
+    }
+    const { t } = useTranslation();
     const googleIcon = <IconBrandGoogleFilled size={20} />;
     const appleIcon = <IconBrandAppleFilled size={20} />;
     const githubIcon = <IconBrandGithubFilled size={20} />;
-    const [type, toggle] = useToggle(['login', 'register']);
+    const [type, toggle] = useToggle([t('login'), t('register')]);
     const [opened, setOpened] = useState(false);
     const [email, setEmail] = useState('');
     const form = useForm({
@@ -56,16 +56,17 @@ export default function LoginPage(props: PaperProps) {
 
     return (
         <Paper radius="md" p="xl" withBorder {...props}>
-            <Text size="lg" fw={500} style={{marginBottom: '1rem'}}>
-                Welcome to Immortal Vault, {type} with
+            <LanguageSwitcher/>
+            <Text size="lg" fw={500} style={{ marginBottom: '1rem' }}>
+                {t('welcome', { type })}
             </Text>
 
             <form onSubmit={form.onSubmit(() => {})}>
                 <Stack>
                     {type === 'register' && (
                         <TextInput
-                            label="Name"
-                            placeholder="Your name"
+                            label={t('name')}
+                            placeholder={t('your_name')}
                             value={form.values.name}
                             onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
                             radius="md"
@@ -74,57 +75,59 @@ export default function LoginPage(props: PaperProps) {
 
                     <TextInput
                         required
-                        label="Email"
-                        placeholder="hello@mantine.dev"
+                        label={t('email')}
+                        placeholder="example@gmail.com"
                         value={form.values.email}
                         onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
-                        error={form.errors.email && 'Invalid email'}
+                        error={form.errors.email && t('invalid_email')}
                         radius="md"
                     />
 
                     <PasswordInput
                         required
-                        label="Password"
-                        placeholder="Your password"
+                        label={t('password')}
+                        placeholder={t('your_password')}
                         value={form.values.password}
                         onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
-                        error={form.errors.password && 'Password should include at least 6 characters'}
+                        error={form.errors.password && t('password_error')}
                         radius="md"
                     />
                 </Stack>
 
                 <Group justify="space-between" mt="xl">
                     <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
-                        {type === 'register'
-                            ? 'Already have an account? Login'
-                            : "Don't have an account? Register"}
+                        {type ==='register' ? t('already_have_account') : t('dont_have_account')}
                     </Anchor>
                     <Button type="submit" radius="xl">
                         {upperFirst(type)}
                     </Button>
                 </Group>
-                {type === 'register' && (<Anchor href="#" size="sm" mt="xs" style={{textAlign:"center"}}  display="block" onClick={() => setOpened(true)}>
-                    Забыли пароль?
-                </Anchor>)}
 
-                <Divider label="Or continue with" labelPosition="center" my="lg" />
+                {type === 'register' && (
+                    <Anchor href="#" size="sm" mt="xs" style={{ textAlign: "center" }} display="block" onClick={() => setOpened(true)}>
+                        {t('forgot_password')}
+                    </Anchor>
+                )}
+
+                <Divider label={t('or_continue_with')} labelPosition="center" my="lg" />
 
                 <Group grow mb="md" mt="md">
-                    <Button /*style={{color: "black", backgroundColor: "grey"}}*/ variant="transparent" leftSection={googleIcon}>Google</Button>
-                    <Button /*style={{color: "black", backgroundColor: "grey"}}*/ variant="transparent" leftSection={appleIcon}>Apple</Button>
-                    <Button /*style={{color: "black", backgroundColor: "grey"}}*/ variant="transparent" leftSection={githubIcon}>GitHub</Button>
+                    <Button variant="transparent" leftSection={googleIcon}>Google</Button>
+                    <Button variant="transparent" leftSection={appleIcon}>Apple</Button>
+                    <Button variant="transparent" leftSection={githubIcon}>GitHub</Button>
                 </Group>
-                <Modal opened={opened} onClose={() => setOpened(false)} title="Восстановление пароля" radius="md" closeOnClickOutside={false} closeOnEscape={false}>
-                <TextInput
-                    radius="md"
-                    label="Введите вашу почту"
-                    placeholder="example@mail.com"
-                    value={email}
-                    onChange={(event) => setEmail(event.currentTarget.value)}
-                    required
-                />
-                <Button mt="md" ml="auto" display="flex" radius="xl" onClick={handlePasswordReset}>Восстановить</Button>
-            </Modal>
+
+                <Modal opened={opened} onClose={() => setOpened(false)} title={t('password_recovery')} radius="md" closeOnClickOutside={false} closeOnEscape={false}>
+                    <TextInput
+                        radius="md"
+                        label={t('enter_email')}
+                        placeholder="example@mail.com"
+                        value={email}
+                        onChange={(event) => setEmail(event.currentTarget.value)}
+                        required
+                    />
+                    <Button mt="md" ml="auto" display="flex" radius="xl" onClick={handlePasswordReset}>{t('recover')}</Button>
+                </Modal>
             </form>
         </Paper>
     );
