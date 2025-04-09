8b18435bf9acd4500624eb47ceb5fefe811f2430
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2025-04-09 12:32:38 +0300

    feat: improved logger, improved ci, autogenerated latest.json, version bump

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use std::env;
+use dotenv::dotenv;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,9 +9,15 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    dotenv().ok();
+
+    let app_stage = env::var("VITE_APP_STAGE").unwrap_or_else(|_| "production".to_string());
+
     tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(
+        .plugin({
+            let now = chrono::Local::now().format("%Y-%m-%d %H:%M:%S");
+
             tauri_plugin_log::Builder::new()
                 .clear_targets()
                 .target(tauri_plugin_log::Target::new(
@@ -19,14 +28,18 @@ pub fn run() {
                 .max_file_size(50_000)
                 .rotation_strategy(tauri_plugin_log::RotationStrategy::KeepAll)
                 .level(log::LevelFilter::Info)
-                .format(|out, message, record| {
-                    out.finish(format_args!("[{}] {}", record.level(), message))
+                .format(move |out, message, record| {
+                    if app_stage == "production" {
+                        out.finish(format_args!("[{}] [{}] {}", now, record.level(), message))
+                    } else {
+                        out.finish(format_args!("[{}] [{}] {} - {}", now, record.level(), record.target(), message))
+                    }
                 })
-                .build(),
-        )
+                .build()
+        })
         .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_opener::init())
-//         .plugin(tauri_plugin_process::init())
+        // .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
             #[cfg(desktop)]
